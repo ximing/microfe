b6af2fb9581d9156d673ec0b22489a2787664fa9
ximing <807754634@qq.com>
2019-04-09 01:28:03 +0800

    feat: support clean

--- packages/cli/lib/config/babel.js
+++ packages/cli/lib/config/babel.js
@@ -1,67 +1,60 @@
 module.exports = {
-    presets: [
-        [
-            require('@babel/preset-env').default,
-            {
-                targets: {
-                    browsers: ['last 4 versions', 'ie >= 10']
-                },
-                modules: false,
-                debug: true,
-                spec: false,
-                loose: false,
-                useBuiltIns: false,
-                include: [],
-                exclude: [],
-                forceAllTransforms: false
-            }
-        ],
-        require('@babel/preset-react').default,
-        [
-            require('@babel/preset-typescript').default,
-            {
-                isTSX: true,
-                allExtensions: true
-            }
-        ]
+  presets: [
+    [
+      require ('@babel/preset-env').default,
+      {
+        targets: {
+          browsers: ['last 4 versions', 'ie >= 10'],
+        },
+        modules: false,
+        debug: false,
+        spec: false,
+        loose: false,
+        useBuiltIns: false,
+        include: [],
+        exclude: [],
+        forceAllTransforms: false,
+      },
     ],
-    ignore: ['node_modules/**', 'dist'],
-    plugins: [
-        [
-            require('babel-plugin-import').default,
-            {
-                libraryName: 'antd',
-                style: true
-            }
-        ],
-        require('@babel/plugin-syntax-optional-chaining').default,
-        require('@babel/plugin-proposal-object-rest-spread').default,
-        require('@babel/plugin-transform-runtime').default,
-        require('@babel/plugin-syntax-dynamic-import').default,
-        require('@babel/plugin-syntax-import-meta').default,
-        require('@babel/plugin-proposal-class-properties').default,
-        require('@babel/plugin-proposal-json-strings').default,
-        [
-            require('@babel/plugin-proposal-decorators').default,
-            {
-                legacy: true
-            }
-        ],
-        require('@babel/plugin-proposal-function-sent').default,
-        require('@babel/plugin-proposal-export-namespace-from').default,
-        require('@babel/plugin-proposal-numeric-separator').default,
-        require('@babel/plugin-proposal-throw-expressions').default,
-        require('@babel/plugin-proposal-export-default-from').default,
-        require('@babel/plugin-proposal-logical-assignment-operators').default,
-        require('@babel/plugin-proposal-optional-chaining').default,
-        [
-            require('@babel/plugin-proposal-pipeline-operator').default,
-            {
-                proposal: 'minimal'
-            }
-        ],
-        require('@babel/plugin-proposal-nullish-coalescing-operator').default,
-        require('@babel/plugin-proposal-do-expressions').default,
-        require('@babel/plugin-proposal-function-bind').default
-    ]
+    require ('@babel/preset-react').default,
+  ],
+  ignore: ['node_modules/**', 'dist'],
+  plugins: [
+    [
+      require ('babel-plugin-import').default,
+      {
+        libraryName: 'antd',
+        style: true,
+      },
+    ],
+    require ('@babel/plugin-syntax-optional-chaining').default,
+    require ('@babel/plugin-proposal-object-rest-spread').default,
+    require ('@babel/plugin-transform-runtime').default,
+    require ('@babel/plugin-syntax-dynamic-import').default,
+    require ('@babel/plugin-syntax-import-meta').default,
+    require ('@babel/plugin-proposal-class-properties').default,
+    require ('@babel/plugin-proposal-json-strings').default,
+    [
+      require ('@babel/plugin-proposal-decorators').default,
+      {
+        legacy: true,
+      },
+    ],
+    require ('@babel/plugin-proposal-function-sent').default,
+    require ('@babel/plugin-proposal-export-namespace-from').default,
+    require ('@babel/plugin-proposal-numeric-separator').default,
+    require ('@babel/plugin-proposal-throw-expressions').default,
+    require ('@babel/plugin-proposal-export-default-from').default,
+    require ('@babel/plugin-proposal-logical-assignment-operators').default,
+    require ('@babel/plugin-proposal-optional-chaining').default,
+    [
+      require ('@babel/plugin-proposal-pipeline-operator').default,
+      {
+        proposal: 'minimal',
+      },
+    ],
+    require ('@babel/plugin-proposal-nullish-coalescing-operator').default,
+    require ('@babel/plugin-proposal-do-expressions').default,
+    require ('@babel/plugin-proposal-function-bind').default,
+  ],
 };
--- packages/cli/lib/config/webpack.common.config.js
+++ packages/cli/lib/config/webpack.common.config.js
@@ -2,112 +2,139 @@
  * Created by yeanzhi on 16/7/19.
  */
 'use strict';
-const webpack = require('webpack');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require ('webpack');
+const CleanWebpackPlugin = require ('clean-webpack-plugin');
+const MiniCssExtractPlugin = require ('mini-css-extract-plugin');
 let env = process.env.NODE_ENV || 'development';
-const babel = require('./babel');
-const postcss = require('./postcss');
+const babel = require ('./babel');
+const postcss = require ('./postcss');
 
-module.exports = ({
+module.exports = (
+  {
     externals = [
-        /^lodash$/,
-        /^jquery$/,
-        /^single-spa$/,
-        /^react$/,
-        /^react\/lib.*/,
-        /^react-dom$/,
-        /.*react-dom.*/,
-        /^rxjs\/?.*$/
+      /^lodash$/,
+      /^jquery$/,
+      /^single-spa$/,
+      /^react$/,
+      /^react\/lib.*/,
+      /^react-dom$/,
+      /.*react-dom.*/,
+      /^rxjs\/?.*$/,
     ],
     devMode = false,
     themer = {},
-    output = 'dist',
-    devtool
-} = {}) => {
-    let cleanDist = output;
-    if (typeof output !== 'string') {
-        cleanDist = output.path;
-    }
-    return {
-        devtool,
-        module: {
-            rules: [
-                {
-                    test: /\.(js|jsx|ts|tsx)(\?.*)?$/,
-                    exclude: /(node_modules)/,
-                    use: {
-                        loader: 'babel-loader',
-                        options: Object.assign(
-                            {},
-                            {
-                                babelrc: false,
-                                configFile: false,
-                                compact: false
-                            },
-                            babel
-                        )
-                    }
-                },
-                {
-                    test: /\.(png|jpe?g|gif|svg|mp4|webm|ogg|mp3|wav|flac|aac|woff2?|eot|ttf|otf)(\?.*)?$/,
-                    use: [
-                        {
-                            loader: 'url-loader',
-                            options: {
-                                limit: 8192
-                            }
-                        }
-                    ]
-                },
-                {
-                    test: /\.worker\.js$/,
-                    use: {
-                        loader: 'worker-loader',
-                        options: { inline: true, fallback: false }
-                    }
-                },
-                {
-                    test: /\.(css|less)(\?.*)?$/,
-                    use: [
-                        devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-                        {
-                            loader: 'css-loader'
-                        },
-                        {
-                            loader: 'postcss-loader',
-                            options: postcss
-                        },
-                        {
-                            loader: 'less-loader',
-                            options: {
-                                modifyVars: themer,
-                                javascriptEnabled: true
-                            }
-                        }
-                    ]
-                }
-            ]
+    output,
+    devtool,
+  } = {}
+) => {
+  let cleanDist = output;
+  if (typeof output !== 'string') {
+    cleanDist = output.path;
+  }
+  console.log ('cleanDist', cleanDist);
+  return {
+    devtool,
+    module: {
+      rules: [
+        {
+          test: /\.(js)x?$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: Object.assign (
+              {},
+              {
+                babelrc: false,
+                configFile: false,
+                compact: false,
+              },
+              babel
+            ),
+          },
         },
-        resolve: {
-            extensions: ['.tsx', '.ts', '.js', '.jsx']
+        {
+          test: /\.(ts)x?$/,
+          exclude: /(node_modules)/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: Object.assign (
+                {},
+                {
+                  babelrc: false,
+                  configFile: false,
+                  compact: false,
+                },
+                babel
+              ),
+            },
+            {
+              loader: 'ts-loader',
+            },
+          ],
         },
-        node: {
-            fs: 'empty'
+        {
+          test: /\.(png|jpe?g|gif|svg|mp4|webm|ogg|mp3|wav|flac|aac|woff2?|eot|ttf|otf)(\?.*)?$/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 8192,
+              },
+            },
+          ],
         },
-        plugins: [
-            new MiniCssExtractPlugin({
-                filename: devMode ? '[name].css' : '[name].[hash].css',
-                chunkFilename: devMode ? '[id].css' : '[id].[hash].css'
-            }),
-            new CleanWebpackPlugin([cleanDist]),
-            new webpack.DefinePlugin({
-                'process.env': {
-                    NODE_ENV: JSON.stringify(env)
-                }
-            }),
-            new webpack.ProvidePlugin({})
-        ],
-        externals
-    };
+        {
+          test: /\.worker\.js$/,
+          use: {
+            loader: 'worker-loader',
+            options: {inline: true, fallback: false},
+          },
+        },
+        {
+          test: /\.(css|less)(\?.*)?$/,
+          use: [
+            devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'postcss-loader',
+              options: postcss,
+            },
+            {
+              loader: 'less-loader',
+              options: {
+                modifyVars: themer,
+                javascriptEnabled: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js', '.jsx', '.json'],
+    },
+    node: {
+      fs: 'empty',
+    },
+    plugins: [
+      new MiniCssExtractPlugin ({
+        filename: devMode ? '[name].css' : '[name].[hash].css',
+        chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+      }),
+      new CleanWebpackPlugin ([cleanDist], {
+        root: process.cwd (),
+        dangerouslyAllowCleanPatternsOutsideProject: true,
+      }),
+      new webpack.DefinePlugin ({
+        'process.env': {
+          NODE_ENV: JSON.stringify (env),
+        },
+      }),
+      new webpack.ProvidePlugin ({}),
+    ],
+    externals,
+  };
 };
