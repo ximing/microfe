bab86c33344ff1e013f18bab5ce7c69c76146c20
ximing <807754634@qq.com>
2019-04-19 23:35:54 +0800

    feat: change findConfig

--- packages/cli/lib/findConfig.js
+++ packages/cli/lib/findConfig.js
@@ -1,13 +1,14 @@
-const path = require('path');
-const chalk = require('chalk');
+const path = require ('path');
+const chalk = require ('chalk');
 
-module.exports = function(configPath) {
-    let config = {};
-    try {
-        configPath = path.resolve(process.cwd(), configPath);
-        config = require(configPath);
-    } catch (e) {
-        console.warn(chalk.yellow(`没有找到自定义config: ${configPath},使用默认配置`));
-    }
-    return config;
+module.exports = function (configPath) {
+  let config = {};
+  try {
+    configPath = path.resolve (process.cwd (), configPath);
+    config = require (configPath);
+  } catch (e) {
+    console.warn (chalk.yellow (`没有找到自定义config: ${configPath},使用默认配置`));
+    console.error (e);
+  }
+  return config;
 };
